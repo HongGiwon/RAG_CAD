84db9c4130c7e0b4486f5317e4192bfbcb4e123b
HongGiwon <gch02518@kaist.ac.kr>
2024-03-05 03:51:02 +0000

    Create RAG_nq_open_run_test.py

---
+++ nq_open/RAG_nq_open_run_test.py
@@ -0,0 +1,63 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from typing import Tuple, Optional, Any, List
+from datasets import load_dataset
+from tqdm import tqdm
+import pickle
+import json
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model_name", type=str, default="meta-llama/Llama-2-70b-chat-hf",
+                        help="Name of pretrained model")
+    parser.add_argument("--max_seq_len", type=int, default=4096, help="max seq len")
+    parser.add_argument("--max_gen_len", type=int, default=64, help="max gen len")
+    parser.add_argument("--num_retrieved_docs", type=int, default=5, help="num_of_retrieved_docs")
+
+    args = parser.parse_args()
+    return args
+
+def get_ll2_model(model_name: str, dtype: torch.dtype = torch.bfloat16) -> Tuple[Any, Any]:
+    model_kwargs = {}
+    model_kwargs['torch_dtype'] = dtype
+    model_kwargs['device_map'] = 'auto'
+    access_token = "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"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="/mnt/ceph_rbd/mistral7b/pretrained/", use_auth_token=access_token)
+    model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir="/mnt/ceph_rbd/mistral7b/pretrained/", use_auth_token=access_token, **model_kwargs)
+
+    model.eval()
+    return tokenizer, model
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    num_retrieved_docs = args.num_retrieved_docs
+    data_path = "prompts/rag_nq_" +str(num_retrieved_docs)+ "_chat_short.json"
+    output_path = "outputs/rag_nq_" +str(num_retrieved_docs)+ "_" + args.model_name.split("/")[-1] + "_short.json"
+    model_name = args.model_name
+    
+    max_seq_len = args.max_seq_len
+    max_ans_len = args.max_gen_len
+    tokenizer, model = get_ll2_model(model_name)
+    #device = "cuda"
+    device = model.device
+
+    with open(data_path, 'r') as f:
+        input_prompts = json.load(f)
+
+    output_prompts = []
+    for prompt in tqdm(input_prompts):
+        encodeds = tokenizer.apply_chat_template(prompt, return_tensors="pt", max_length=max_seq_len)
+        model_inputs = encodeds.to(device)
+        
+        with torch.inference_mode():
+            generated_ids = model.generate(model_inputs, max_new_tokens=max_ans_len, do_sample=True)
+        
+        generated_ans = tokenizer.decode(generated_ids[0][len(model_inputs[0]):], skip_special_tokens=True)
+        output_prompts.append(generated_ans)
+        
+    with open(output_path, 'w') as f:
+        json.dump(output_prompts, f)

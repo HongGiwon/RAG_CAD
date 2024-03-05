ce23a92a105ed430ede1391b4e47f77eeaaa251a
HongGiwon <gch02518@kaist.ac.kr>
2024-03-05 03:48:29 +0000

    Update RAG_nq_open_promptgen_chat_short.py

--- nq_open/src/RAG_nq_open_promptgen_chat_short.py
+++ nq_open/src/RAG_nq_open_promptgen_chat_short.py
@@ -38,7 +38,7 @@ def gen_prompt(input_data, num_retrieved_docs):
     return prompt
 
 
-num_retrieved_docs = 10
+num_retrieved_docs = 5
 data_path = '../download/nq-open-10_total_documents_gold_at_0.jsonl.gz'
 output_path = '../prompts/rag_nq_' + str(num_retrieved_docs) + '_chat_short.json'
 

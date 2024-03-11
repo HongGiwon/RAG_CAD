6d2107cdad9392fc1d90333a7172bc84c8130a71
user name <s2523062-infk8s@eidf029-host1.vms.os.eidf.epcc.ed.ac.uk>
2024-03-11 04:10:02 +0000

    len10_short

--- nq_open/src/RAG_nq_open_cad_promptgen_chat_short.py
+++ nq_open/src/RAG_nq_open_cad_promptgen_chat_short.py
@@ -43,8 +43,8 @@ def gen_prompt_single(input_data, target_doc_id):
     return prompt
 
 
-num_retrieved_docs = 5
-ans_pos = 9
+num_retrieved_docs = 10
+ans_pos = 4
 data_path = '../download/nq-open-10_total_documents_gold_at_' + str(ans_pos) + '.jsonl.gz'
 output_path = '../prompts/rag_nq_' + str(num_retrieved_docs) + '_cad_chat_short_' + str(ans_pos) + '.json'
 

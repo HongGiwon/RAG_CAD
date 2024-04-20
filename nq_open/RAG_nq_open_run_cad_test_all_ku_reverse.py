e4895b4db01a4949d7e732a66d4784ece2acf934
user name <s2523062-infk8s@eidf029-host1.vms.os.eidf.epcc.ed.ac.uk>
2024-04-20 01:22:36 +0000

    fix3?

--- nq_open/RAG_nq_open_run_cad_test_all_ku_reverse.py
+++ nq_open/RAG_nq_open_run_cad_test_all_ku_reverse.py
@@ -81,6 +81,7 @@ if __name__ == "__main__":
     device = model.device
     space_tok_id = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(": "))[-1]
     #model.generation_config.pad_token_id = model.generation_config.eos_token_id
+    tokenizer.pad_token = tokenizer.pad_token or tokenizer.eos_token
 
     with open(data_path, 'r') as f:
         input_prompts = json.load(f)
